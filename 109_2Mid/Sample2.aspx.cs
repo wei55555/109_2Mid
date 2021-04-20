f92f0e532e27756237f77b30392a569415c73d6b
昱瑋 李 <107111226@mail.oit.edu.tw>
2021-04-20 15:24:57 +0800

    1-a

--- 109_2Mid/Sample2.aspx.cs
+++ 109_2Mid/Sample2.aspx.cs
@@ -8,7 +8,8 @@ using System.Web.UI.WebControls;
 namespace _109_2Mid {
     public partial class Sample2 : System.Web.UI.Page 
     {
-        protected void Page_Load(object sender, EventArgs e) {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
         }
 
